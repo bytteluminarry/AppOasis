d1a6c68e4712c9d39ab59816b5c936eac6300736
Ayman <aaaaaymanatif@gmail.com>
2024-03-09 00:08:06 +0000

    Animtion

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,13 +25,49 @@ function closeLeftNav() {
     }, 300);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("DOMContentLoaded", function () {
-        var lazyImages = document.querySelectorAll(".lazy-load");
+const elementsToHide = document.querySelectorAll('#main-informative *');
+elementsToHide.forEach((element, index) => {
+    if (index > 0)
+        element.classList.add('hidden');
+});
 
-        lazyImages.forEach(function (lazyImage) {
-            lazyImage.setAttribute("loading", "lazy");
-            lazyImage.src = lazyImage.dataset.src;
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyImages = document.querySelectorAll(".lazy-load");
+
+    lazyImages.forEach(function (lazyImage) {
+        lazyImage.setAttribute("loading", "lazy");
+        lazyImage.src = lazyImage.dataset.src;
     });
 });
+
+// Function to handle animation
+function handleAnimation(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Select all children of main-informative and show them
+            const children = entry.target.querySelectorAll('*');
+            let interval = 100;
+            children.forEach(child => {
+                setTimeout(() => {
+                    child.classList.add('animate');
+                }, interval);
+                interval += 40;
+            });
+            // Unobserve the target once animation is applied
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(handleAnimation, {
+    root: null, // use the viewport as the root
+    rootMargin: '0px', // no margin
+    threshold: 0.3 // trigger when 30% of the element is visible
+});
+
+// Observe the main-informative section
+observer.observe(document.getElementById('main-informative'));
+
+// Show the main-informative section
+document.getElementById('main-informative').style.opacity = '1';
